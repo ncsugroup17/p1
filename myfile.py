fdcdca8c48ae955eb2d167ebd5ce17d175772b50
MesekerWK <63201911+MesekerWK@users.noreply.github.com>
2025-01-13 16:14:44 +0000

    This is the 10line file with an error

---
+++ myfile.py
@@ -0,0 +1,10 @@
+def greet_user(name):
+    print("Hello, " + name)
+
+user_name = "Alice"
+age = 25
+
+if age > 18
+    greet_user(user_name)  # SyntaxError due to missing colon
+else:
+    print("You are too young.")
