0dc077aa75bd3af6c41036e55cbefb38bd12713f
MesekerWK <63201911+MesekerWK@users.noreply.github.com>
2025-01-21 15:44:05 +0000

    Fixed the error after completing step6

--- myfile.py
+++ myfile.py
@@ -4,7 +4,7 @@ def greet_user(name):
 user_name = "Alice"
 age = 25
 
-if age > 18
+if age > 18:
     greet_user(user_name)  # SyntaxError due to missing colon
 else:
     print("You are too young.")
